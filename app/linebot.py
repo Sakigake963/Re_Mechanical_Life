2465c6c75332d7aa2d1c4b907d4ab851dccd8f0a
TakumaMatsubara <tamosongyuan3@gmail.com>
2023-06-13 21:11:40 +0900

    startproject実行

--- app/linebot.py
+++
@@ -1,9 +0,0 @@
-#LINEBotに関わる処理
-
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import time
-import requests
-import datetime, re
-
